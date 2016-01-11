aaefc9e556fc6565d03a098744caf33d6fdf85e9
liuwencai <821272852@qq.com>
2016-01-11 22:36:59 +0800

    add baidu map sdk

---
+++ app/src/main/java/com/chengying/root/map/BaiduMapActivity.java
@@ -0,0 +1,14 @@
+package com.chengying.root.map;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BaiduMapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       // SDKInitializer.initialize(getApplicationContext());
+       // setContentView(R.layout.activity_baidu_map);
+    }
+}
